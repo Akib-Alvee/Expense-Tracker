a93f5b15c8d332c4b2f362fc6304ccc4912591d5
Akib-Alvee <akibulislam632@gmail.com>
2021-10-29 17:21:11 +0600

    test

--- expensewebsite/expensewebsite/settings.py
+++ expensewebsite/expensewebsite/settings.py
@@ -132,3 +132,11 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MESSAGE_TAGS = {
     messages.ERROR : 'danger'
 }
+
+#email
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
